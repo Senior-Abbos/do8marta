d29f3d471d29615822e7f5fb3608f3d5a4a835e2
Senior-Abbos <imabboskhamidov@gmail.com>
2024-03-07 20:44:03 +0500

    добавили инструкцию к git

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ array.forEach(element => {
 
 // git init - инициализировать новый проект
 // 1. git status
-// 2. git add - добавляет файл к репозиторию
-
-
+// 2. git add - добавляет файл к репозиторию 
+// 3. git commit -m 'Ваше сообщение'
+// 4. git push -u origin master
+// 5. один раз до push - git remote add origin 'link'
 // Опубликовать
